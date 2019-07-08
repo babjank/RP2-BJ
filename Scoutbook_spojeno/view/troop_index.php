659b4c3b34095f13fc5735382b05c95335d94f4c
lojelen <lojelen@student.math.hr>
2019-07-08 23:20:03 +0200

    Obrisani nepotrebni fileovi.(3)

--- Scoutbook_spojeno/view/troop_index.php
+++
@@ -1,6 +0,0 @@
-<?php require_once "view/_header.php"; ?>
-
-<canvas id="cnv" height="600" width="600"></canvas>
-<script src="./scripts/troop.js?newversion"></script>
-
-<?php require_once "view/_footer.php"; ?>
