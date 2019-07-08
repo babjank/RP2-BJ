c26f7c8ed54320d63063e11f6bc19a2655536d51
lojelen <lojelen@student.math.hr>
2019-07-08 22:13:03 +0200

    Obrisani nepotrebni fileovi.

--- Zadaca2/view/register_success.php
+++
@@ -1,8 +0,0 @@
-<?php require_once "view/_loginHeader.php"; ?>
-
-<p>
-	Registracija je uspješno provedena.<br>
-	Sada se možete ulogirati na <a href="teamup.php">početnoj stranici</a>.
-</p>
-
-<?php require_once "view/_footer.php"; ?>
